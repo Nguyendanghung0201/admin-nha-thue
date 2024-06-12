64a74c8a6b091c8873ba69ac77aeeac0f6a1ba08
danghung <danghung0201@gmail.com>
2024-06-12 21:28:04 +0700

    add

--- index.js
+++ index.js
@@ -736,7 +736,7 @@ async function crawler3(urls) {
             .get(),
     );
     const traffic_map = $(
-        '.container-information-traffic-right img',
+        '.container-information-traffic-right-map img',
     ).attr('src');
     console.log('ajajj ', traffic_map)
     const traffic_coordinates_map = JSON.stringify(
